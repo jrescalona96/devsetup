03df26f3faf653417d52bb6f38ff3117a2a178e8
John Escalona <johnrichmondescalona@gmail.com>
2025-04-06 10:43:40 -0700

    add fzf

--- dev_env/.config/nvim/init.lua
+++ dev_env/.config/nvim/init.lua
@@ -674,7 +674,7 @@ require("lazy").setup({
 			--        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
 			local servers = {
 				-- clangd = {},
-				-- gopls = {},
+				gopls = {},
 				-- pyright = {},
 				-- rust_analyzer = {},
 				-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -916,7 +916,8 @@ require("lazy").setup({
 			-- Load the colorscheme here.
 			-- Like many other themes, this one has different styles, and you could load
 			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight-night")
+			-- vim.cmd.colorscheme("tokyonight-night")
+			vim.cmd.colorscheme("retrobox")
 		end,
 	},
 
